62666d2376214552102128aee427a6ba210bc55d
team04yien <s10256100@connect.np.edu.sg>
2024-01-24 00:32:39 +0800

    setting up files

---
+++ mainSolution.py
@@ -0,0 +1,6 @@
+# Team = Group 4 
+# Members = Choy Yi En, Lim Yin Shan, Chan Jia Wei, Tan jia Heng 
+# Student id = S10256100D, 
+# Class number = TC09 
+user_email = "s10256100@connect.np.edu.sg"
+user_name = "yien"
