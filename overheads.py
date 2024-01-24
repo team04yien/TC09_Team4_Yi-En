96da04fcd6a1ab3b5395b1bcf946bf5409b38efa
team04yien <s10256100@connect.np.edu.sg>
2024-01-24 12:06:03 +0800

    create and append

--- overheads.py
+++ overheads.py
@@ -10,4 +10,11 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader) # skip header
 
-    # create an empty list for overheads record 
+    # create an empty list for overheads record
+    overheadsRecords = [] 
+
+    # append overhead record into the overheadRecords list
+    for row in reader:
+        category = row[0]
+        overhead = float(row[1])  # Convert overhead to float for numerical operations
+        overheadsRecords.append([category, overhead])   
