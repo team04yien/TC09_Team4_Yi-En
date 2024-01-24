8f5f304df0e836f8e826df6e694032a07b1476bf
team04yien <s10256100@connect.np.edu.sg>
2024-01-24 15:04:47 +0800

    read file

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -5,3 +5,7 @@ import csv
 fp = Path.cwd()/"csv_reports"/"Cash_on_Hand.csv"
 print(fp.exists())
 
+# read the csv file 
+with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+    reader = csv.reader(file)
+    next(reader) # skip header
