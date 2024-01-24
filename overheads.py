300503a924d6ce1e972b78b8ddcb6233c7863938
team04yien <s10256100@connect.np.edu.sg>
2024-01-24 13:04:13 +0800

    change to dictionary

--- overheads.py
+++ overheads.py
@@ -10,11 +10,25 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader) # skip header
 
-    # create an empty list for overheads record
-    overheadsRecords = [] 
+# Go through the rows, 
+    for row in reader: 
+        # name the according rows
+        category = row[0] 
+        overhead = float(row[1]) #float it as we need numberic operation 
 
-    # append overhead record into the overheadRecords list
-    for row in reader:
-        category = row[0]
-        overhead = float(row[1])  # Convert overhead to float for numerical operations
-        overheadsRecords.append([category, overhead])   
+# create a dictionary for total overheads
+        total_overheads = float({}) #float everything in the dictionary 
+
+# sum up overheads by category 
+        
+        
+ 
+ 
+    # # create an empty list for overheads record
+    # overheadsRecords = [] 
+
+    # # append overhead record into the overheadRecords list
+    # for row in reader:
+    #     category = row[0]
+    #     overhead = float(row[1])  # Convert overhead to float for numerical operations
+    #     overheadsRecords.append([category, overhead])    
