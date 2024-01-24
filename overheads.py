b728dc87e4dab6e4d7fc0d6da2e797dffb35f760
team04yien <s10256100@connect.np.edu.sg>
2024-01-24 11:37:06 +0800

    read csv file

--- overheads.py
+++ overheads.py
@@ -4,3 +4,10 @@ import csv
 # create a file path to csv file.
 fp = Path.cwd()/"csv_reports"/"Overheads.csv"
 print(fp.exists())
+
+# read the csv file 
+with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+    reader = csv.reader(file)
+    next(reader) # skip header
+
+    # create an empty list for overheads record 
