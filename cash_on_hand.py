fd03425bce25b68dc93cef93dfd67922d953fa10
team04yien <s10256100@connect.np.edu.sg>
2024-01-25 12:00:21 +0800

    got the differences

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -9,11 +9,23 @@ print(fp.exists())
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader) # skip header
+
+    # Read the first data row to initialize previous_cash
+    first_data_row = next(reader) 
+    pervious_cash = float(first_data_row[1])
+
 # Go through the rows, 
     for row in reader: 
         # name the according rows
         day = row[0] 
         cash_on_hand = float(row[1]) #float it as we need numberic operation
- 
-pervious_cash_amount = 0 
-current_cash_amount = float(row[1]) 
+        current_cash = cash_on_hand
+        difference = current_cash - pervious_cash  
+        print (difference)
+
+
+
+# # Initial first day to make it $0 
+# pervious_cash_amount = row[0] 
+# current_cash_amount = float(row[1]) 
+# difference = current_cash_amount - pervious_cash_amount 
